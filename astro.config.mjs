c2ea6229cccafd9801e9324090cfc6438dbc21fd
Diego <diegomartinfnz@gmail.com>
2024-11-27 18:42:35 +0100

    commit

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
-export default defineConfig({
-	site: 'https://DiegoMfer.github.io',
-	integrations: [mdx(), sitemap()],
-});
